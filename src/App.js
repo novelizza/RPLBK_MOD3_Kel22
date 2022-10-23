3c220016494aa29338a12f0550ddc53193533c65
novelizza <novel.izza.fardana@gmail.com>
2022-10-23 12:47:09 +0700

    Nouvel Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
-function App() {
+import Card from "./component/card";
+
+export default function App() {
+  const [data, setData] = React.useState([]);
+  const [tema, setTema] = React.useState("");
+
+  React.useEffect(() => {
+    const topik = [
+      "indonesia",
+      "business",
+      "world",
+      "academia/opinion",
+      "life",
+      "travel",
+      "front-row",
+      "sports",
+    ];
+    const now = new Date();
+
+    async function restData() {
+      const getData = await axios
+        .get(
+          "https://jakpost.vercel.app/api/category/" +
+            topik[now.getSeconds() % topik.length]
+        )
+        .then(
+          (data) => setData(data.data.posts),
+          setTema(topik[now.getSeconds() % topik.length])
+        )
+        .catch((er) => console.log(er.response));
+
+      return getData;
+    }
+
+    restData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        "flex-wrap": "wrap",
+        "flex-direction": "column",
+        "align-content": "center",
+      }}
+    >
+      <h1 style={{ width: 450, textAlign: "center" }}>Kelompok 22</h1>
+      <p style={{ width: 450, textAlign: "center", fontWeight: "bold" }}>
+        News About {tema === "academia/opinion" ? "opini" : tema}
+      </p>
+      {data.map((item, i) => {
+        return <Card key={i} data={item} />;
+      })}
     </div>
   );
 }
-
-export default App;
